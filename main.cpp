20d6cee508e38b8185fcd2f6bbe8cb6c600ebcd1
Andrew Dionizio <98423649+adionizio14@users.noreply.github.com>
2022-11-30 12:26:24 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,19 +7,24 @@ int main() {
     std::cout << "How many decks are you playing with?" << std::endl;
     myTree.num_decks = myTree.getDecks();
     std::cout << std::endl;
-    myTree.fill_initial();
+    myTree.fill_initialA();
+    myTree.fill_initialS();
     myTree.start = 0;
     myTree.end = 12;
     myTree.current = 1;
+    myTree.lSeg = 0;
+    myTree.rSeg = 6;
     myTree.create_segTree(myTree.start, myTree.end, myTree.current);
     /*for(int i = 0; i < 13; i++){
         std::cout << myTree.initial_array[i] << " ";
     }
-    std::cout << std::endl;*/
-    for(int i = 0; i < 26; i++){
+    std::cout << std::endl;
+    for(int i = 0; i < 32; i++){
         std::cout << myTree.segment_tree[i] << " ";
-    }
+    }*/
 
+    myTree.segSum = myTree.findSegSum(myTree.start, myTree.end, myTree.current, myTree.lSeg, myTree.rSeg);
+
+    std::cout << myTree.segSum ;
 
 }
-

93d434b90d7f093839f921231f331f9b2701bd43
Andrew Dionizio <98423649+adionizio14@users.noreply.github.com>
2022-11-30 01:39:40 -0500

    Update SegTree.cpp

--- SegTree.cpp
+++ SegTree.cpp
@@ -19,10 +19,8 @@ void SegmentTree::fill_initial() {
 }
 
 void SegmentTree::create_segTree(int start, int end, int current) {
+    std::cout << "[" << start << ":" << end << "]" <<  " " << current << std::endl;
 
-    /*std::cout << std::endl;
-    std::cout << "[" << start << ":" << end << "]" <<  " " << current << std::endl;*/
-    //std::cout << std::endl;
 
     if (start == end){
         segment_tree[current] = initial_array[start];
@@ -31,10 +29,26 @@ void SegmentTree::create_segTree(int start, int end, int current) {
 
 
     else{
+        int left_current;
+        int right_current;
         int left_end = (start + end)/2;
         int right_start = ((start + end)/2) + 1;
-        int left_current = (current*2) + 1;
-        int right_current = (current*2) + 2;
+
+        if (current*2 >= 24){
+            if(current*2 == 24){
+                left_current = (2*current - 2);
+                right_current = (2*current - 2)  + 1;
+            }
+            else{
+                left_current = (2*current - (2*current - 24));
+                right_current = (2*current - (2*current - 24)) + 1;
+            }
+
+        }
+        else {
+            left_current = (current*2) ;
+            right_current = (current*2) + 1;
+        }
         create_segTree(start, left_end, left_current);
         create_segTree(right_start, end, right_current);
         segment_tree[current] = segment_tree[left_current] + segment_tree[right_current];
@@ -42,6 +56,10 @@ void SegmentTree::create_segTree(int start, int end, int current) {
     }
 
 }
+
+
+
+
 
 
 

12cb232d918ca738a53896dec389847613645585
Andrew Dionizio <98423649+adionizio14@users.noreply.github.com>
2022-11-30 01:40:03 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,15 +10,16 @@ int main() {
     myTree.fill_initial();
     myTree.start = 0;
     myTree.end = 12;
-    myTree.current = 0;
+    myTree.current = 1;
     myTree.create_segTree(myTree.start, myTree.end, myTree.current);
     /*for(int i = 0; i < 13; i++){
         std::cout << myTree.initial_array[i] << " ";
     }
     std::cout << std::endl;*/
-    for(int i = 0; i < 25; i++){
+    for(int i = 0; i < 26; i++){
         std::cout << myTree.segment_tree[i] << " ";
     }
 
 
 }
+

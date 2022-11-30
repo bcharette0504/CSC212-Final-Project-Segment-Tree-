fbadc1832adc36b9234949dce36a3d82da9f23f7
Andrew Dionizio <98423649+adionizio14@users.noreply.github.com>
2022-11-30 01:40:23 -0500

    Update SegTree.h

--- SegTree.h
+++ SegTree.h
@@ -5,10 +5,11 @@ public:
     int num_decks;
     int num_cards;
     int initial_array[13];
-    int segment_tree[25] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0, 0, 0, 0};
+    int segment_tree[26] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0, 0, 0, 0,0};
     int start;
     int end;
     int current;
+
 
 
 

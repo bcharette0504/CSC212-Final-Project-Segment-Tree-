4752ea3b1e058875fa0751b19f1c7ac11c6a5df8
Andrew Dionizio <98423649+adionizio14@users.noreply.github.com>
2022-12-01 10:55:26 -0500

    Update SegTree.h

--- SegTree.h
+++ SegTree.h
@@ -4,8 +4,9 @@ class SegmentTree {
 public:
     int num_decks;
     int num_cards;
+    int size;
     int initial_array[13];
-    int segment_tree[32];
+    int segment_tree[13*4];
     int start;
     int end;
     int current;
@@ -18,6 +19,7 @@ public:
 
     void create_segTree(int start, int end, int current);
     int findSegSum(int start, int end, int current, int lSeg, int rSeg);
+    void probOfDealerBetter(int usersvalue, int dealersvalue);
     int getDecks();
     void fill_initialA();
     void fill_initialS();

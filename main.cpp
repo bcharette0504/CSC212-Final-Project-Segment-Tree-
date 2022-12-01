9c2fd7e078156ada01ded7cb9f527d1cefd7af10
Andrew Dionizio <98423649+adionizio14@users.noreply.github.com>
2022-11-30 20:43:23 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 int main() {
 
     SegmentTree myTree;
+    myTree.size = 13;
     std::cout << "How many decks are you playing with?" << std::endl;
     myTree.num_decks = myTree.getDecks();
     std::cout << std::endl;
@@ -12,8 +13,7 @@ int main() {
     myTree.start = 0;
     myTree.end = 12;
     myTree.current = 1;
-    myTree.lSeg = 0;
-    myTree.rSeg = 6;
+
     myTree.create_segTree(myTree.start, myTree.end, myTree.current);
     /*for(int i = 0; i < 13; i++){
         std::cout << myTree.initial_array[i] << " ";
@@ -22,9 +22,11 @@ int main() {
     for(int i = 0; i < 32; i++){
         std::cout << myTree.segment_tree[i] << " ";
     }*/
-
+    std::cout << "Input range you would like to find: " << std::endl;
+    std::cin >> myTree.lSeg >> myTree.rSeg;
     myTree.segSum = myTree.findSegSum(myTree.start, myTree.end, myTree.current, myTree.lSeg, myTree.rSeg);
 
-    std::cout << myTree.segSum ;
+    std::cout << std::endl;
+    std::cout << "Sum = " << myTree.segSum ;
 
 }

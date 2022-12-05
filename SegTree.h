03fb4ca8805df3c86e9832c14005129e95e8d047
Andrew Dionizio <98423649+adionizio14@users.noreply.github.com>
2022-12-05 18:32:43 -0500

    Add files via upload

---
+++ SegTree.h
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+class SegmentTree {
+public:
+    int num_decks;
+    int num_cards;
+    int size;
+    int initial_array[13];
+    int segment_tree[13*4];
+    int start;
+    int end;
+    int current;
+    int lSeg;
+    int rSeg;
+    int segSum;
+
+
+
+
+    void create_segTree(int i, int left, int right);
+    int findSegSum(int start, int end, int current, int lSeg, int rSeg);
+    void probOfDealerBetter(int usersvalue, int dealersvalue);
+    void probOfNotGoingOver21(int usersvalue);
+    void outputGraph(std::string fname);
+    int getDecks();
+    void fill_initialA();
+    void fill_initialS();
+    int sum(int left, int right);
+
+};
